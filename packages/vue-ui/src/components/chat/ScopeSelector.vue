176f7467a94f70d9b24803d47463b2c2bc6a0087
Maxime Haas <maxime.haas@viacesi.fr>
2026-09-09 00:25:35 +0200

    fix: restore 'Aktuelle Zelle' scope button

--- packages/vue-ui/src/components/chat/ScopeSelector.vue
+++ packages/vue-ui/src/components/chat/ScopeSelector.vue
@@ -10,7 +10,7 @@ const emit = defineEmits<{
 const { isLoading } = useAiTutorStore()
 const { t } = useI18n()
 
-const scopes: FeedbackScope[] = ['task', 'sheet']
+const scopes: FeedbackScope[] = ['cell', 'task', 'sheet']
 </script>
 
 <template>
